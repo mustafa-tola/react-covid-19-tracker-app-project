2c5028ab5f42d7f0118661ce8c0dbfe6bbc48d72
mustafa-tola <46423331+mustafa-tola@users.noreply.github.com>
2020-07-02 11:11:11 +0500

    Update Chart.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { fetchDailyCovidData } from "../../api/index"
-import { Line, Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import styles from "./Chart.module.css";
 
 export const Chart = () => {
